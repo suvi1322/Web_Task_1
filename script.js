6baf624031ecae6a26b547c428410bfab4e714a9
suvi1322 <161008430+suvi1322@users.noreply.github.com>
2025-07-14 20:55:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('infoButton').addEventListener('click', function() {
+  alert("Augmented Reality helps students and users learn about cultural festivals through immersive experiences!");
+});
